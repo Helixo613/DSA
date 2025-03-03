0b41b3a8c53463b07bfbaf585bce3e0ef7487d53
Arnav Bansal <arnavbansal613@gmail.com>
2025-03-03 18:51:32 +0530

    maps.cpp & inheritence_intro.cpp

--- maps.cpp
+++ maps.cpp
@@ -10,30 +10,31 @@ using namespace std;
 
 int main() {
     int n;
-    cin>>n;
-    map<string,int>m;
-    for(int i=0; i<n; i++){
+    cin >> n; // Read number of queries
+    map<string, int> m; // Map to store names and associated values
+
+    for (int i = 0; i < n; i++) {
         int q;
-        cin>>q;
-        
-        if (q==1){
+        cin >> q; // Read query type
+        string name;
+
+        if (q == 1) { 
             int num;
-            string name;
-            cin>>name>>num;
-            m[name]+=num;
-        }
-        if (q==2){
-            string name;
-            cin>>name;
-            m.erase(name);
-        }
-        if (q==3){
-            string name;
-            cin>>name;
-            cout<<m[name]<<endl;
+            cin >> name >> num;
+            m[name] += num; // Directly update the value
+        } 
+        else if (q == 2) { 
+            cin >> name;
+            auto it = m.find(name); 
+            if (it != m.end()) m.erase(it); // Only erase if the name exists
+        } 
+        else if (q == 3) { 
+            cin >> name;
+            auto it = m.find(name);
+            cout << (it != m.end() ? it->second : 0) << endl; // Print value if found, else print 0
         }
     }
-  
+
     return 0;
 }
 
